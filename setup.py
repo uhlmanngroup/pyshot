a130ab764072fc771575760ed96b202bdca67dc8
Julien Jerphanion <git@jjerphan.xyz>
2022-01-01 15:56:02 +0100

    Bump to 1.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name="pyshot",
     author="Julien Jerphanion",
     author_email="git@jjerphan.xyz",
-    version='1.1',
+    version='1.2',
     cmdclass={'build_ext': build_ext},
     ext_modules=extensions,
     install_requires=[
